9c793ded17f5044b9d9150aeca4771baccdd6a58
Kirill Liubun <kreol_123@ukr.net>
2020-04-02 20:29:34 +0300

    added interfaces

--- algorithms/main.js
+++ algorithms/main.js
@@ -1,4 +1,19 @@
+// exercises #1, implement algorithms finding greatest common divisor (GCD)
+function gcd(a, b) {
+    //TODO
+    return 1;
+}
+
+// exercises #2, implement algorithms finding least common multiple (LCM)
+function lcm(a, b) {
+    //TODO
+    return 1;
+}
+
+// exercises #3, inverse given array
 function inverseArray(arr) {
     //TODO
     return arr;
 }
+
+
