5298fb4d849798a44b7bcc9eb447f8bf0ea9ce90
Pavel Liubun <qeqeqed4@gmail.com>
2020-07-02 06:02:59 -0700

    implement function gcd

--- algorithms/main.js
+++ algorithms/main.js
@@ -1,18 +1,26 @@
 // exercises #1, implement algorithms finding greatest common divisor (GCD)
 function gcd(a, b) {
-    //TODO
-    return 1;
+     while (a != 0 && b != 0){
+	 if( a > b) { 
+	 a = a % b 
+	 } else ( b > a ){
+		 b = b % a 
+}
+	 return a || b ;
+}
 }
 
 // exercises #2, implement algorithms finding least common multiple (LCM)
 function lcm(a, b) {
-    //TODO
-    return 1;
+     for (let j = a; ; j++){
+		if (a%b == 0) continue;
+	 return Math.min(j);
+	 }
 }
 
 // exercises #3, inverse given array
 function inverseArray(arr) {
-    //TODO
+    
     return arr;
 }
 
